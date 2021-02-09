778f5957f5d9e7d6f23981a35026e975e7770d22
leidy2608 <78742991+leidy2608@users.noreply.github.com>
2021-02-09 00:00:12 -0400

    Ejercicio practico primer corte

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int a = 1;
+		int b = 2;
+		int c = 3;
+
+	//Variables abc se escriben aacb
+		Console.WriteLine("1132");
+	//Variables abc se escriben bca
+		Console.WriteLine("123");
+	//Variables abc se escriben bcbc
+		Console.WriteLine("2323");
+	}
+}
